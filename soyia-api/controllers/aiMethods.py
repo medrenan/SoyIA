00d5f42aaf70d9e556d80b55f869c54c978827af
carolinam <carolinamargiott2i@gmail.com>
2022-10-03 10:11:06 -0300

    criado json para todas as amostras e plantas

--- soyia-api/controllers/aiMethods.py
+++ soyia-api/controllers/aiMethods.py
@@ -1,4 +1,17 @@
 from flask import make_response,jsonify
 
 def getGrainsEstimate(cultiveId):
-    return make_response({"plant1":20},200)
+    return make_response({
+        "sampleOne":{
+            "plant1":20,
+            "plant2":20,
+        },
+        "sampleTow":{
+            "plant1":20,
+            "plant2":20,
+        },
+        "sampleThree":{
+            "plant1":20,
+            "plant2":20,
+        }
+    },200)
