eda50fdf81147f31f63e1556339240904098b30f
Rafael Bettinidias <rafael8885326@gmail.com>
2022-10-29 19:03:29 -0300

    adcionado mais imagens ao dataset

---
+++ IA/treino.py
@@ -0,0 +1,15 @@
+#importar rede
+from darkflow.net.build import TFNet
+#opções do treino
+options = {"model": "cfg/yolo-new.cfg", 
+           "load": "bin/yolo.weights",
+           "batch": 2,
+           "epoch": 5000,
+           "gpu": 1.0,
+           "train": True,
+           "annotation": "./annotations/",
+           "dataset": "./images/"}
+#configurando a rede com as opções
+tfnet = TFNet(options)
+#treino
+tfnet.train()
