db98726ebc7f9a79bd5c3381c73b5d57d72a1f9f
carolinam <carolinamargiott2i@gmail.com>
2022-10-31 10:32:57 -0300

    adicionado mensagens de erro e configuracoes

--- soyia-api/db/firebase.py
+++ soyia-api/db/firebase.py
@@ -1,18 +1,35 @@
 from firebase_admin import credentials, initialize_app,get_app
 
 def initializeApp():
-    credential = credentials.Certificate('YOUR DOWNLOADED CREDENTIALS FILE (JSON)')
-    options= {
-        "databaseURL":"",
-        'storageBucket': 'YOUR FIREBASE STORAGE PATH (without gs://)'
-    }
+    try:
+        credential = credentials.Certificate('YOUR DOWNLOADED CREDENTIALS FILE (JSON)')
+        options= {
+            "databaseURL":"https://soyia-api-default-rtdb.firebaseio.com",
+            'storageBucket': 'soyia-api.appspot.com'
+        }
 
-    return initialize_app(credential, options)
+        app = initialize_app(credential, options)
+        print("iniciado firebase com sucesso")
+        return app
+    except:
+        print("Houve um erro ao iniciar firebase")
+        pass
 
 def firebase_setup():
     try:
-        return get_app()
+        app = get_app();
+        print("iniciado firebase com sucesso")
+        return app
     except:
+        print("Houve um erro ao iniciar firebase")
         pass
     return initializeApp()
         
+# const firebaseConfig : any = {
+#     apiKey: "AIzaSyA539sEfikhamS_I6WCBbVTYNMo3Btbtgs",
+#     appId: "1:625773996693:android:170e3ff34b7f5e58b4c99c",
+#     projectId: "soyia-api",
+#     databaseURL: "https://soyia-api-default-rtdb.firebaseio.com",
+#     messagingSenderId: '625773996693',
+#     storageBucket: "soyia-api.appspot.com",
+# };
