e7dde872e5ed836c8ac6d93804bafd88ea77e687
carolinam <carolinamargiott2i@gmail.com>
2022-10-31 10:43:24 -0300

    arrumado geracao do nome do arquivo e implementado

--- soyia-api/controllers/upload.py
+++ soyia-api/controllers/upload.py
@@ -3,16 +3,18 @@ from datetime import date
 from flask import make_response
 
 def filePath(file,folderPath):
-    extension = file.name.split('.').pop();
-    fileName = f'${date.today()}.${extension}'
-    return f'${folderPath}/${fileName}'
+    extension = file.filename.split('.').pop();
+    fileName = f'{date.today()}.{extension}'
+    return f'{folderPath}/{fileName}'
 
 def fileUpload(file,folderPath):
     try:
+        fileName = filePath(file,folderPath)
+
         from firebase_admin import storage
         bucket = storage.bucket()
-        blob = bucket.blob(file.name)
-        blob.upload_from_filename(file.name)
+        blob = bucket.blob(fileName)
+        blob.upload_from_filename(fileName)
         blob.make_public()
         print("your file url", blob.public_url)
     except:
