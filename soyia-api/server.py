69e851bcd7db8166b9ccfa55e35c3a1e745117ac
carolinam <carolinamargiott2i@gmail.com>
2022-10-31 10:09:51 -0300

    registrado blueprint do fileupload

--- soyia-api/server.py
+++ soyia-api/server.py
@@ -1,13 +1,14 @@
 from flask import Flask,request
 from flask_cors import CORS
 from models.envVariables import port,host
-from controllers import general_bp,aiMethods_bp
+from controllers import general_bp,aiMethods_bp,fileUpload_bp
 from db.firebase import firebase_setup
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(general_bp)
 app.register_blueprint(aiMethods_bp)
+app.register_blueprint(fileUpload_bp)
 
 firebase_setup()
 
