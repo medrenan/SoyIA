7a34bd6d69e906228dc83baa199ff1b010dce732
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2022-11-01 16:32:30 -0300

    arrumado firebase

--- soyia-api/controllers/upload.py
+++ soyia-api/controllers/upload.py
@@ -1,5 +1,4 @@
 from datetime import date
-
 from flask import make_response
 
 def filePath(file,folderPath):
@@ -10,15 +9,14 @@ def filePath(file,folderPath):
 def fileUpload(file,folderPath):
     try:
         fileName = filePath(file,folderPath)
-
         from firebase_admin import storage
         bucket = storage.bucket()
         blob = bucket.blob(fileName)
-        blob.upload_from_filename(fileName)
+        blob.upload_from_file(file)
         blob.make_public()
-        print("your file url", blob.public_url)
-    except Exception as inst:
-        print(inst.args)
+        res = make_response({"mensagem":"imagem baixada em: "+blob.public_url},200)
+        return res
+    except:
         res = make_response({"mensagem":"Houve um erro ao tentar fazer upload da mensagem"},400)
         return res
 
--- soyia-api/db/firebase.py
+++ soyia-api/db/firebase.py
@@ -1,15 +1,28 @@
 import firebase_admin
 from firebase_admin import initialize_app,get_app,credentials
+from models.envVariables import privateKey,privateKeyId,projectId,clientEmail,clientId,clientCertUrl
 
 def initializeApp():
     try:
-        cred = credentials.Certificate("./service_account.json")
+        cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": projectId,
+        "private_key_id": privateKeyId,
+        "private_key": privateKey,
+        "client_email": clientEmail,
+        "client_id": clientId,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": clientCertUrl
+        })
+
         options= {
             "databaseURL":"https://soyia-api-default-rtdb.firebaseio.com",
             'storageBucket': 'soyia-api.appspot.com'
         }
 
-        app = firebase_admin.initialize_app(cred)
+        app = firebase_admin.initialize_app(cred,options)
         print("iniciado firebase com sucesso")
         return app
     except:
@@ -24,12 +37,3 @@ def firebase_setup():
     except:
         pass
     return initializeApp()
-
-# const firebaseConfig : any = {
-#     apiKey: "AIzaSyA539sEfikhamS_I6WCBbVTYNMo3Btbtgs",
-#     appId: "1:625773996693:android:170e3ff34b7f5e58b4c99c",
-#     projectId: "soyia-api",
-#     databaseURL: "https://soyia-api-default-rtdb.firebaseio.com",
-#     messagingSenderId: '625773996693',
-#     storageBucket: "soyia-api.appspot.com",
-# };
