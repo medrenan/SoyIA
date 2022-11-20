aab54a6a993dd44809b3832b730163dc8185bbae
Felipe Silva <ffelipe.ssilva02@gmail.com>
2022-11-20 13:43:24 -0300

    title fixed

--- IA/confusionMatrix.py
+++ IA/confusionMatrix.py
@@ -11,11 +11,11 @@ for i in range(30):
     search+=str(i)
     imagemN=(realDet[search])
     print("JSON: ",imagemN)
-    
     array = [[imagemN['graosReais'],imagemN['graosDetectados']],[imagemN['vagensReais'],imagemN['vagensDetectadas']]]
     print(array)
     df_cm = pd.DataFrame(array, index = ['Vagens','Grãos'],columns = ['Reais','Detectados'])
     plt.figure(figsize = (2,2))
     plt.ticklabel_format(style='plain', useOffset=False)
     sn.heatmap(df_cm, annot=True, fmt='g')
+    plt.title(search)
     plt.show()
